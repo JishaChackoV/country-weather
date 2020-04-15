c7bab883c3b20b466296e800783b99991caf1165
JishaChackoV <jishtechversant@gmail.com>
2020-04-15 20:10:05 +0530

    weather details

--- src/CapitalWeather.js
+++ src/CapitalWeather.js
@@ -1,51 +1,48 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-class CapitalWeather extends Component{
-   constructor(props){
-       super(props);
-           this.state={
-                
-             
-           }
-   }
-   onSubmit=()=>{
-       const access="1ea015744997294d300d6cd2e57ec920"
-        axios.get(`http://api.weatherstack.com/current? access_key=${access}&query =${this.props.capital}`)
-    .then(res => {
-     const persons = res.data;
-     this.setState({ weatherDatas:persons });
-   })
-   }
+class CapitalWeather extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
- 
-    render(){
-        
+        }
+    }
+    onSubmit = () => {
+
+        axios.get(`http://api.weatherstack.com/current?access_key=69187d964fd1574bbd9472b7b570f0ee&query=${this.props.capital}`)
+            .then(res => {
+                const persons = res.data;
+                this.setState({ weatherDatas: persons });
+            })
+    }
+    render() {
+
         const { datas } = this.props
-        console.log("propss",)
-        return(
-        <>
-        <table>
-           <tr><th>temperature  </th>
-            <th>weather_icons</th>
-            <th>wind_speed</th>
-            <th>precip</th>
+        console.log("propss")
+        return (
+            <>
+                <table>
+                    <tr><th>temperature  </th>
+                        <th>weather_icons</th>
+                        <th>wind_speed</th>
+                        <th>precip</th>
 
-            </tr>
-            <tr>
-        <td>{}</td>
-        <td>{}</td>
-        <td>{}</td>
-            <td><img  alt="flag" style={{width:"50px"}}/></td>
+                    </tr>
+                    <tr>
+                        <td>{this.state.weatherDatas.current.description}</td>
+                        <td><img src={this.state.weatherDatas.current.weather_icons} alt="flag" style={{ width: "50px" }} /></td>
+                        <td>{this.state.weatherDatas.wind_speed}</td>
+                        <td>{this.state.weatherDatas.precip}</td>
 
-            </tr>
+                    </tr>
 
 
-        </table>
-        <button onClick={this.onSubmit}>Capital Weather</button>
+                </table>
+                <button onClick={this.onSubmit}>Capital Weather</button>
 
-        </>)
+            </>)
     }
 }
 export default CapitalWeather
